20c7b146dcb75450f72a1cc3e971b527e30b3cbd
Tekleselassie Leul <edenlove2019@gmail.com>
2020-08-24 14:35:34 -0700

    first

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navbar from './component/Navbar';
+import Card from './component/Card';
+import CardList from './component/CardList';
+const App = () => {
+	return (
+		<>
+			<Navbar />
+			<CardList />
+		</>
+	);
+};
+
+export default App;
